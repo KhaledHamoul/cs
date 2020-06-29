da98fa77eb60f0af3e3e278b5cf34f1890658e30
khaledHamoul <fk_hamoul@esi.dz>
2020-06-29 20:25:33 +0200

    creating datasets models and migrations

--- app/models.py
+++ app/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Attribute(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    label = models.CharField(max_length=200)
+    description = models.TextField()
+    attribute_type = models.CharField(max_length=30)
+
+class Record(models.Model):
+    data = models.TextField()
+
+class Dataset(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    # foreign keys
+    parent = models.ForeignKey('Dataset', on_delete=models.DO_NOTHING, null=True)
+    attributes = models.ManyToManyField(Attribute)
+    records = models.ManyToManyField(Record)
+
+
 
