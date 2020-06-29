2c757c1773011adc1ed5337b5c84a9fa27846d5a
khaledHamoul <fk_hamoul@esi.dz>
2020-06-29 20:27:40 +0200

    some updates & custom stuff

--- app/views.py
+++ app/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
 from django import template
+from django.template.response import TemplateResponse
+from pprint import pprint
+from logging import warning
+from app.models import Dataset
+from django.db.models import Count 
+
 
 @login_required(login_url="/login/")
 def index(request):
@@ -11,20 +17,34 @@ def index(request):
 @login_required(login_url="/login/")
 def pages(request):
     context = {}
-    # All resource paths end in .html.
-    # Pick out the html file name from the url. And load that template.
     try:
-        
-        load_template = request.path.split('/')[-1]
-        html_template = loader.get_template( load_template )
+        load_template = request.path[1:]
+        html_template = loader.get_template(load_template + '.html')
         return HttpResponse(html_template.render(context, request))
-        
+
     except template.TemplateDoesNotExist:
 
-        html_template = loader.get_template( 'error-404.html' )
-        return HttpResponse(html_template.render(context, request))
+        try:
+            load_template = request.path.split('/')[-1]
+            html_template = loader.get_template(load_template)
+            return HttpResponse(html_template.render(context, request))
+
+        except template.TemplateDoesNotExist:
+            html_template = loader.get_template('errors/error-404.html')
+            return HttpResponse(html_template.render(context, request))
+
+        except:
+
+            html_template = loader.get_template('errors/error-500.html')
+            return HttpResponse(html_template.render(context, request))
 
     except:
-    
-        html_template = loader.get_template( 'error-500.html' )
+
+        html_template = loader.get_template('errors/error-500.html')
         return HttpResponse(html_template.render(context, request))
+
+
+@login_required(login_url="/login/")
+def data_index(request):
+    datasets = Dataset.objects.annotate(records_count=Count("records")).all()
+    return render(request, "data/index.html", {'datasets': datasets})
--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'  # Enable the inner app 
+    'rest_framework',
+    'app'
 ]
 
 MIDDLEWARE = [
